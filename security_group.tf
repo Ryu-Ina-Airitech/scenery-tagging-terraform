cb8e751f7e1c4bf8de22c68c6dbcfc34bb83feb1
Ryutaro Inatsu <ryutaro.inatsu@airitech.co.jp>
2022-02-17 17:58:30 +0900

    Delete useless security group settings

--- security_group.tf
+++ security_group.tf
@@ -33,13 +33,42 @@ resource "aws_security_group_rule" "scenery-tagging-web-sgr_in_https" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "scenery-tagging-web-sgr_out_tcp3000" {
+resource "aws_security_group_rule" "scenery-tagging-web-sgr_out_http" {
+  security_group_id = aws_security_group.scenery-tagging-opmg-sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "scenery-tagging-web-sgr_out_https" {
+  security_group_id = aws_security_group.scenery-tagging-opmg-sg.id
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "scenery-tagging-app-sgr_out_tcp3000" {
   security_group_id        = aws_security_group.scenery-tagging-web-sg.id
   type                     = "egress"
   protocol                 = "tcp"
   from_port                = 3000
   to_port                  = 3000
-  source_security_group_id = aws_security_group.scenery-tagging-app-sg.id
+  # source_security_group_id = aws_security_group.scenery-tagging-app-sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "scenery-tagging-app-sgr_out_tcp5000" {
+  security_group_id        = aws_security_group.scenery-tagging-web-sg.id
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 5000
+  to_port                  = 5000
+  # source_security_group_id = aws_security_group.scenery-tagging-app-sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 # app security
@@ -55,41 +84,25 @@ resource "aws_security_group" "scenery-tagging-app-sg" {
   }
 }
 
-resource "aws_security_group_rule" "scenery-tagging-app-sgr_in_tcp3000" {
-  security_group_id        = aws_security_group.scenery-tagging-app-sg.id
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = 3000
-  to_port                  = 3000
-  source_security_group_id = aws_security_group.scenery-tagging-web-sg.id
+resource "aws_security_group_rule" "scenery-tagging-web-sgr_in_tcp3000" {
+  security_group_id = aws_security_group.scenery-tagging-web-sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 3000
+  to_port           = 3000
+  # source_security_group_id = aws_security_group.scenery-tagging-app-sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
-# resource "aws_security_group_rule" "scenery-tagging-app-sgr_out_http" {
-#   security_group_id = aws_security_group.scenery-tagging-app-sg.id
-#   type              = "egress"
-#   protocol          = "tcp"
-#   from_port         = 80
-#   to_port           = 80
-#   prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
-# }
-
-# resource "aws_security_group_rule" "scenery-tagging-app-sgr_out_https" {
-#   security_group_id = aws_security_group.scenery-tagging-app-sg.id
-#   type              = "egress"
-#   protocol          = "tcp"
-#   from_port         = 443
-#   to_port           = 443
-#   prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
-# }
-
-# resource "aws_security_group_rule" "scenery-tagging-app-sgr_out_tcp3306" {
-#   security_group_id        = aws_security_group.scenery-tagging-app-sg.id
-#   type                     = "egress"
-#   protocol                 = "tcp"
-#   from_port                = 3306
-#   to_port                  = 3306
-#   source_security_group_id = aws_security_group.scenery-tagging-db-sg.id
-# }
+resource "aws_security_group_rule" "scenery-tagging-web-sgr_in_tcp5000" {
+  security_group_id = aws_security_group.scenery-tagging-web-sg.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 5000
+  to_port           = 5000
+  # source_security_group_id = aws_security_group.scenery-tagging-app-sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
 # operation and management security
 resource "aws_security_group" "scenery-tagging-opmg-sg" {
@@ -110,55 +123,5 @@ resource "aws_security_group_rule" "scenery-tagging-opmg-sgr_in_ssh" {
   protocol          = "tcp"
   from_port         = 22
   to_port           = 22
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["133.32.176.165/32"]
 }
-
-resource "aws_security_group_rule" "scenery-tagging-opmg-sgr_in_tcp3000" {
-  security_group_id = aws_security_group.scenery-tagging-opmg-sg.id
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = 3000
-  to_port           = 3000
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "scenery-tagging-web-sgr_out_http" {
-  security_group_id = aws_security_group.scenery-tagging-opmg-sg.id
-  type              = "egress"
-  protocol          = "tcp"
-  from_port         = 80
-  to_port           = 80
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "scenery-tagging-web-sgr_out_https" {
-  security_group_id = aws_security_group.scenery-tagging-opmg-sg.id
-  type              = "egress"
-  protocol          = "tcp"
-  from_port         = 443
-  to_port           = 443
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-# # db security
-# resource "aws_security_group" "scenery-tagging-db-sg" {
-#   name        = "${var.project}-${var.environment}-db-sg"
-#   description = "db role security group"
-#   vpc_id      = aws_vpc.vpc-scenery-tagging.id
-
-#   tags = {
-#     Name    = "${var.project}-${var.environment}-db-sg"
-#     Project = var.project
-#     Env     = var.environment
-#   }
-# }
-
-# resource "aws_security_group_rule" "scenery-tagging-db-sgr_in_tcp3306" {
-#   security_group_id        = aws_security_group.scenery-tagging-db-sg.id
-#   type                     = "ingress"
-#   protocol                 = "tcp"
-#   from_port                = 3306
-#   to_port                  = 3306
-#   source_security_group_id = aws_security_group.scenery-tagging-app-sg.id
-# }
-
